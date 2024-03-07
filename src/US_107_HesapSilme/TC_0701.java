1c4361269154a153587b10aa154d2859531cb2f1
sammycaglar <caglarsemih1@outlook.com>
2024-03-08 01:48:47 +0300

    Eksikler Giderildi

--- src/US_107_HesapSilme/TC_0701.java
+++ src/US_107_HesapSilme/TC_0701.java
@@ -15,14 +15,14 @@ public class TC_0701 extends BaseDriver {
         // Navigate to the login page
         driver.findElement(By.xpath("//a[text()='Giriş Yap']")).click();
         // Enter the credentials and click the "Giriş Yap" button
-        driver.findElement(By.cssSelector("input[id='life']")).sendKeys("testnomads01@gmail.com");
-        driver.findElement(By.cssSelector("input[id='lifp']")).sendKeys("Nomad07++");
+        driver.findElement(By.cssSelector("input[id='life']")).sendKeys("SeleniumProjectTeam@gmail.com");
+        driver.findElement(By.cssSelector("input[id='lifp']")).sendKeys("Bugfighters1234!");
         driver.findElement(By.xpath("//input[@id='lfb']")).click();
         // Navigate to the user profile and click the "Hesabımı Sil" button
         driver.findElement(By.linkText("Test")).click();
         driver.findElement(By.linkText("Hesabımı Sil")).click();
         // Enter the password for account deletion confirmation
-        driver.findElement(By.cssSelector("input[id='p']")).sendKeys("Nomad07++" + Keys.ENTER);
+        driver.findElement(By.cssSelector("input[id='p']")).sendKeys("Bugfighters1234!" + Keys.ENTER);
         // Find the confirmation message and check if the account deletion is successful
         WebElement condition = driver.findElement(By.cssSelector("div[id='C4w'] i"));
         Assert.assertEquals("Hesap silinemedi!", "Hesabın silindi", condition.getText().substring(0, 15));
